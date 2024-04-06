e0c603b2e4a43e59cb51e491d49f943f786aff28
Tiago <tiagobnarita@gmail.com>
2024-04-06 19:17:12 -0300

    criating error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    //se for uma instancia de erro
+  }
+});
 
 app.listen(3333, () => console.log("servidor on-line !!!"));
