9ae82be7fe045934e49925acf2648b56cea6f958
Tiago <tiagobnarita@gmail.com>
2024-04-08 23:24:03 -0300

    creating users routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
 
+import { CreateUserController } from "./controllers/user/CreateUserControllers";
+
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("Error to make a request");
-});
+//-- Rotas USERS --
+router.get("/user", new CreateUserController().handle);
 
 export { router };
