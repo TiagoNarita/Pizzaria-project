310f90251bcda1194d5f0252d2b76264abd35745
Tiago <tiagobnarita@gmail.com>
2024-04-11 00:36:07 -0300

    .,

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserControllers";
 import AuthUserController from "./controllers/user/AuthUserController";
 import DetailUserController from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -11,5 +12,5 @@ router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
-router.get("/me", new DetailUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 export { router };
