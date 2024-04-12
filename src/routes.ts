82e34fec298f54e75fa5c32f35a02ad9508fc1b1
Tiago <tiagobnarita@gmail.com>
2024-04-12 19:47:29 -0300

    creating category service

--- src/routes.ts
+++ src/routes.ts
@@ -13,4 +13,7 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//-- Rotas CATEGORY --
+
 export { router };
---
+++ src/services/category/CreateCategoryServices.ts
@@ -0,0 +1,9 @@
+import primsaClient from "../../prisma";
+
+class CreateCategoryService {
+  async exercute() {
+    return { ok: true };
+  }
+}
+
+export default CreateCategoryService;
