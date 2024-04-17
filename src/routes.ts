54865e0977b4fbd5868ad7b41639bd029953c899
Tiago <tiagobnarita@gmail.com>
2024-04-16 22:24:19 -0300

    send message update

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "./controllers/user/CreateUserControllers";
 import AuthUserController from "./controllers/user/AuthUserController";
 import DetailUserController from "./controllers/user/DetailUserController";
-import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
+//middleware
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer";
 
 const router = Router();
 
+const upload = multer(uploadConfig.upload("./tmp"));
 //-- Routes USERS --
 router.post("/users", new CreateUserController().handle);
 
@@ -29,6 +33,11 @@ router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 // -- Routes PRODUCTS --
 
-router.post("/product", isAuthenticated, new CreateProductController().handle);
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
 
 export { router };
